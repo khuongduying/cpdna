de324dbbab49812ad9f7f5d8152281a4f2ceeb6e
khuongduying <khuongduying@gmail.com>
2024-06-06 10:43:42 +0700

    trim for genemind's sample

--- code/PREPROCESSING/qc_trim.sh
+++ code/PREPROCESSING/qc_trim.sh
@@ -23,3 +23,26 @@ trimmomatic PE \
 
 # re-check QC
 fastqc data/TRIM_FASTQ/*.gz -o out/QC/TRIMMED
+
+# Trims
+# mamba activate dnaseq
+p_raw="/mnt/rdisk/duydao/PROJECT/CPDNA_MANGO/data/FASTQ/GENEMIND/WI1382403A_AW-939"
+p_trim="/mnt/rdisk/duydao/PROJECT/CPDNA_MANGO/data/TRIM_FASTQ/GENEMIND"
+
+trimmomatic PE \
+    -phred33 \
+    -threads 56 \
+    -trimlog $p_trim/GENEMIND.log \
+    -summary $p_trim/GENEMIND_sum.log \
+    $p_raw/202405141519_2305151010_2P240501023US2S2693DX_A_20240514KtestDemo3_WI1382403A01_L00_R1.fq.gz \
+    $p_raw/202405141519_2305151010_2P240501023US2S2693DX_A_20240514KtestDemo3_WI1382403A01_L00_R2.fq.gz \
+    $p_trim/GENEMIND_L00_R1_trimmed_paired.fq.gz \
+    $p_trim/GENEMIND_L00_R1_trimmed_unpaired.fq.gz \
+    $p_trim/GENEMIND_L00_R2_trimmed_paired.fq.gz \
+    $p_trim/GENEMIND_L00_R2_trimmed_unpaired.fq.gz \
+    ILLUMINACLIP:/mnt/rdisk/duydao/PROJECT/DNASEQ_BRCA12/ref/adapters/TruSeq3-PE-2.fa:2:30:10:8:3:true \
+    HEADCROP:10 \
+    CROP:140
+
+# re-check QC
+fastqc data/TRIM_FASTQ/GENEMIND/*.gz -o out/QC/TRIMMED/GENEMIND
